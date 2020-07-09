bb0055bb015b967e4d530d044ac0753fb17509f1
Nick Hart <nhartmusic@gmail.com>
2020-07-09 19:49:03 +0200

    bad code way done, time to fix

--- build/index.js
+++ build/index.js
@@ -1,5 +1,28 @@
 "use strict";
-console.log('i will fuck you up');
-var logSomething = function () {
-    console.log('yeah');
-};
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length; j++) {
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var lefthand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = lefthand;
+                    }
+                }
+                if (typeof this.collection === 'string') {
+                    this.collection.
+                    ;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, -3, 69, 0, 420]);
+sorter.sort();
+console.log(sorter.collection);
