b5588e7cbcb87ad6e2189b7ea8188a72344ccf84
Nick Hart <nhartmusic@gmail.com>
2020-07-10 15:12:03 +0200

    starting to abstract sorting functionality into individual files

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,4 @@
-class Sorter {
-    constructor(public collection: number[] | string | LinkedList) { }
-
-    sort(): void {
-        const { length } = this.collection
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length; j++) {
-
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const lefthand = this.collection[j]
-                        this.collection[j] = this.collection[j + 1]
-                        this.collection[j + 1] = lefthand
-                    }
-                }
-
-                if (typeof this.collection === 'string') {
-                    this.collection.
-                }
-
-            }
-        }
-    }
-}
+import Sorter from './sorter'
 
 const sorter = new Sorter([10, -3, 69, 0, 420])
 sorter.sort()
